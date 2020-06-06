716aaca43cde1973f03786482067f61f1f081c55
Thomas Antony <tantony@purdue.edu>
2020-06-06 16:07:41 -0500

    Added version with progress + image generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use image::{ImageBuffer};
+
 fn main() {
     println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+
+    let mut im = ImageBuffer::new(image_width, image_height);
+    for j in (0..image_height).rev()
+    {
+        eprint!("\rScanlines remaining: {} ", j);
+        for i in 0..image_width
+        {
+            let r = i as f64 / (image_width as f64 - 1.);
+            let g = j as f64 / (image_height as f64 - 1.);
+            let b = 0.25;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            let pixel = image::Rgb([ir, ig, ib]);
+            im.put_pixel(i, image_height-j-1, pixel);
+        }
+    }
+    println!("");
+    im.save("./output.png").unwrap();
 }
